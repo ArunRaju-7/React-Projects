9e136e983bdb69c7a7619d08c26d8c92b33cd030
Arun Raju <63866528+ArunRaju-7@users.noreply.github.com>
2025-11-26 15:41:39 +0000

    Sidebar changes commit

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,11 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Header today="Tuesday" date="25-11-2025" />
+      <div className="main-sections">
+        <div className="sidebar-section">
+          <Sidebar name="Arunachalam" gmail="arunachalamraj06@gmail.com" />
+        </div>
+        <div className="mainbar-section">Body section</div>
+      </div>
     </div>
   );
 }
